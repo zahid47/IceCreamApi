636396bcd69cbaa97d20a1f1b801f7a6b49d35e1
zahid <epiczahid@gmail.com>
2022-05-23 19:05:33 +0600

    added a basic test

--- src/__tests__/icecream.test.ts
+++ src/__tests__/icecream.test.ts
@@ -3,7 +3,7 @@ import app from "../utils/app";
 
 describe("server", () => {
   describe("GET /", () => {
-    describe("given the server is running", () => {
+    describe("given the server is working", () => {
       it("should return a 200", async () => {
         const { statusCode } = await request(app).get(`/`);
         expect(statusCode).toBe(200);
